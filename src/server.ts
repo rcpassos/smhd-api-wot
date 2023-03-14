d2f514a2ee6e173929c3fafb722ed7752fb01dc3
rcpassos <r.passos@outlook.pt>
2023-03-14 23:13:49 +0000

    api key

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,21 @@ const app = fastify();
 
 const databaseClient = new PrismaClient();
 
+interface IParams {
+  serialNumber?: string;
+  id?: string;
+}
+
 app.register(jwt, {
   secret: process.env.JWT_SECRET_KEY as string,
 });
 
-app.addHook("onRequest", async (request, reply) => {
-  // TODO: check the API KEY process.env.API_KEY with x-api-key
+app.addHook("onRequest", (request, reply, done) => {
+  if (process.env.API_KEY !== request.headers["x-api-key"]) {
+    return reply.status(403).send();
+  }
+
+  done();
 });
 
 app.decorate(
@@ -57,7 +66,7 @@ app.post("/auth/register", async (request, reply) => {
   const token = app.jwt.sign(
     { id: user.id, email: user.email },
     {
-      expiresIn: "7d",
+      expiresIn: process.env.JWT_EXPIRES_IN || "1d",
     }
   );
 
@@ -104,30 +113,134 @@ app.post(
   "/devices",
   { onRequest: [app.authenticate] },
   async (request, reply) => {
-    // TODO: create schema
+    const deviceSchema = z.object({
+      serialNumber: z.string(),
+    });
 
-    // TODO: validate body
+    const { serialNumber } = deviceSchema.parse(request.body);
 
-    // TODO: register device
+    const deviceAlreadyExists = await databaseClient.device.findFirst({
+      where: { serialNumber },
+    });
 
-    return reply.status(200).send([]);
+    if (deviceAlreadyExists) {
+      return reply.status(201).send(deviceAlreadyExists);
+    }
+
+    const device = await databaseClient.device.create({
+      data: {
+        serialNumber,
+      },
+    });
+
+    await databaseClient.userDevice.create({
+      data: {
+        userId: request.user.id,
+        deviceId: device.id,
+      },
+    });
+
+    return reply.status(201).send(device);
+  }
+);
+
+app.delete<{
+  Params: IParams;
+}>(
+  "/devices/:id",
+  { onRequest: [app.authenticate] },
+  async (request, reply) => {
+    const { id } = request.params;
+
+    await databaseClient.userDevice.deleteMany({
+      where: {
+        userId: request.user.id,
+        deviceId: id,
+      },
+    });
+
+    await databaseClient.device.delete({
+      where: { id },
+    });
+
+    return reply.status(200).send();
   }
 );
 
 app.get(
-  "/devices/:id/events",
+  "/devices",
   { onRequest: [app.authenticate] },
   async (request, reply) => {
-    // TODO: get device events
+    const userDevices = await databaseClient.userDevice.findMany({
+      where: { userId: request.user.id },
+    });
 
-    return reply.status(200).send([]);
+    const deviceIds = userDevices.map((userDevice) => userDevice.deviceId);
+
+    const devices = await databaseClient.device.findMany({
+      where: {
+        id: { in: deviceIds },
+      },
+    });
+
+    return reply.status(200).send(devices);
   }
 );
 
-app.post("/devices/:id/events", async (request, reply) => {
-  // TODO: create schema
+app.get<{
+  Params: IParams;
+}>(
+  "/devices/:serialNumber/events",
+  { onRequest: [app.authenticate] },
+  async (request, reply) => {
+    const { serialNumber } = request.params;
 
+    if (!serialNumber) {
+      return reply
+        .status(400)
+        .send({ message: "Serial number must be provided" });
+    }
+
+    const device = await databaseClient.device.findFirst({
+      where: { serialNumber },
+    });
+
+    if (!device) {
+      return reply.status(404).send({ message: "Can not find the device" });
+    }
+
+    const deviceEvents = await databaseClient.deviceEvent.findMany({
+      where: { deviceId: device?.id },
+    });
+
+    return reply.status(200).send(deviceEvents);
+  }
+);
+
+app.post<{
+  Params: IParams;
+}>("/devices/:serialNumber/events", async (request, reply) => {
   // TODO: validate body
+
+  const { serialNumber } = request.params;
+
+  if (!serialNumber) {
+    return reply
+      .status(400)
+      .send({ message: "Serial number must be provided" });
+  }
+
+  let device = await databaseClient.device.findFirst({
+    where: { serialNumber },
+  });
+
+  if (!device) {
+    device = await databaseClient.device.create({
+      data: {
+        serialNumber,
+      },
+    });
+  }
 
   // TODO: create device event
 
